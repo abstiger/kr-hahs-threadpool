4aaa77e57475ae1b730935e7fadad23e81917129
Tiger <huzai9999999990@163.com>
2012-03-07 08:52:02 +0800

    let worker sleep one sec

--- kr_threadpool_test.c
+++ kr_threadpool_test.c
@@ -3,19 +3,19 @@
 #include <unistd.h>
 #include <assert.h>
 
-int worker(void * task) 
+int worker(void *task) 
 {
     fprintf(stdout, "worker [%lu] processing task[%s] \n", \
             pthread_self(), (char *)task);
     fflush(stdout);
-    //sleep(1);
+    sleep(1);
     return 0;
 }
 
 static char *task[]={ "task0", "task1", "task2", "task3", "task4", 
-                            "task5", "task6", "task7", "task8", "task9",
-                            "task10", "task11", "task12", "task13", "task14", 
-                            "task15", "task16", "task17", "task18", "task19"};
+                      "task5", "task6", "task7", "task8", "task9",
+                      "task10", "task11", "task12", "task13", "task14", 
+                      "task15", "task16", "task17", "task18", "task19"};
 
 int main(void) 
 {
